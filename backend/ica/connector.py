55a8d4431fc5e9508975dc60f17d696ee048fec7
Patrick Rauscher <prauscher@prauscher.de>
2019-06-05 00:58:10 +0200

    prepare IcaConnector by adding IcaSession

--- backend/ica/connector.py
+++ backend/ica/connector.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3.7
 # -*- coding: utf-8 -*-
 
+from .session import IcaSession
 import datetime
 import random
-import hmac
 
 MEMBERSHIP_ORDINARY = 1
 MEMBERSHIP_SECONDARY = 2
@@ -14,55 +14,26 @@ GENDER_MALE = 369
 GENDER_FEMALE = 370
 GENDER_VARIOUS = 371
 
-ALLOWED_HASHES = [
-    "cb942e814af87e97e5be94c8fd7a3b2b",
-    "8e19eeeb40d7e7ff92f054f099846650",
-]
-
-
-def _calculate_hash(username, password):
-    return hmac.new(username.encode("utf-8"),
-                    password.encode("utf-8")).hexdigest()
-
 
 class IcaConnector:
-    def __init__(self):
-        pass
+    def __init__(self, endpoint):
+        self.endpoint = endpoint
 
     def auth(self, user, password):
-        return _calculate_hash(user, password) in ALLOWED_HASHES
+        session = IcaSession(self.endpoint)
+        return session.auth(user, password)
 
     def search(self, user, password, string):
-        return [
-            {"memberId": random.randint(100, 199), "name": "Philipp Metzler"},
-            {"memberId": random.randint(200, 299), "name": "Random Dude"},
-        ]
+        session = IcaSession(self.endpoint)
+        session.auth(user, password)
+        return session.search(string)
 
     def get(self, user, password, id):
-        return {
-            "givenName": "Philipp",
-            "surname": "Metzler",
-            "gender": GENDER_MALE,
-            "membership": MEMBERSHIP_ORDINARY,
-            "additionalAddress": "",
-            "street": "Mittwaldallee 7",
-            "cityCode": "41561",
-            "city": "Barnbeck",
-            "birthDate": datetime.date(2003, 5, 7),
-            "mail": "metzler@example.com",
-            "mailGuardian": "eltern@example.org",
-            "phoneNumber": "+49 123456",
-            "mobileNumber": "+49 987654",
-            "reason1": "-",
-            "reason2": "-",
-            "membershipDate": datetime.date(2019, 5, 24),
-            "department": "Graue Bären",
-        }
+        session = IcaSession(self.endpoint)
+        session.auth(user, password)
+        return session.get(id)
 
     def activate(self, user, password, memberId):
-        return True
-
-
-if __name__ == "__main__":
-    import sys
-    print(_calculate_hash(sys.argv[1], sys.argv[2]))
+        session = IcaSession(self.endpoint)
+        session.auth(user, password)
+        return session.activate(memberId)
---
+++ backend/ica/session.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import hmac
+
+ALLOWED_HASHES = [
+    "cb942e814af87e97e5be94c8fd7a3b2b",
+    "8e19eeeb40d7e7ff92f054f099846650",
+]
+
+
+def _calculate_hash(username, password):
+    return hmac.new(username.encode("utf-8"),
+                    password.encode("utf-8")).hexdigest()
+
+
+class IcaSession:
+    session_id = None
+
+    def __init__(self, endpoint):
+        self.endpoint = endpoint
+
+    def auth(self, user, password):
+        self.session_id = "AAA"
+        return _calculate_hash(user, password) in ALLOWED_HASHES
+
+    def search(self, string):
+        return [
+            {"memberId": random.randint(100, 199), "name": "Philipp Metzler"},
+            {"memberId": random.randint(200, 299), "name": "Random Dude"},
+        ]
+
+    def get(self, id):
+        return {
+            "givenName": "Philipp",
+            "surname": "Metzler",
+            "gender": GENDER_MALE,
+            "membership": MEMBERSHIP_ORDINARY,
+            "additionalAddress": "",
+            "street": "Mittwaldallee 7",
+            "cityCode": "41561",
+            "city": "Barnbeck",
+            "birthDate": datetime.date(2003, 5, 7),
+            "mail": "metzler@example.com",
+            "mailGuardian": "eltern@example.org",
+            "phoneNumber": "+49 123456",
+            "mobileNumber": "+49 987654",
+            "reason1": "-",
+            "reason2": "-",
+            "membershipDate": datetime.date(2019, 5, 24),
+            "department": "Graue Bären",
+        }
+
+    def activate(self, id):
+        return True
+
+
+if __name__ == "__main__":
+    import sys
+    print(_calculate_hash(sys.argv[1], sys.argv[2]))
